e90089e11f4488eeea61739313a1290ec2142f24
Rob Harding <robbie.harding@hotmail.com>
2017-05-16 11:39:55 -0400

    Added game over screen

---
+++ src/net/robharding/brickbreaker/states/menu/GameOverState.java
@@ -0,0 +1,27 @@
+package net.robharding.brickbreaker.states.menu;
+
+import net.robharding.brickbreaker.states.GameState;
+import net.robharding.brickbreaker.states.GameStateManager;
+
+public class GameOverState extends GameState {
+
+	public GameOverState(GameStateManager gsm) {
+		super(gsm);
+	}
+
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void cleanUp() {
+		
+	}
+
+}
